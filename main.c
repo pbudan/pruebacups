272e799f021cde92c7360daf341a14d0fbd2808a
PaolaB <PaolaB@PaolaB-PC>
2013-03-24 23:43:57 -0300

    esta es la primera versión

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: PaolaB
+ *
+ * Created on 24 de marzo de 2013, 23:36
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
